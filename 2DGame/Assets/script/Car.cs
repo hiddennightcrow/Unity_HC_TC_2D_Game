e01c08907f7e61113d0d7a043ebc82714ff3e107
hiddennightcrow <57255290+hiddennightcrow@users.noreply.github.com>
2020-12-08 21:35:06 +0800

    設定元件

---
+++ 2DGame/Assets/script/Car.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Car : MonoBehaviour
+{
+    [Header("尺寸"),Range(100,150)]
+    public int size = 100;
+    [Header("重量"),Tooltip("車子的重量")]
+    public float weight = 1.5f;
+    [Header("品牌"),Tooltip("車子的品牌")]
+    public string brand = "BMW";
+    [Header("是否有天窗"),Tooltip("車子的天窗")]
+    public bool haswindow = true;
+
+    [Header("顏色")]
+    public Color colorA = Color.blue;
+    public Color colorB = Color.green;
+    public Color colorC = new Color(1, 0, 0);
+    public Color colorD = new Color(1, 0, 0, 0.5f);
+
+    [Header("二維向量")]
+    public Vector2 vecA = Vector2.zero;
+    public Vector2 vecB = Vector2.one;
+    public Vector2 vecC = new Vector2(0, 0);
+
+    public AudioClip soundA;
+    public Sprite picA;
+    public GameObject trfA;
+    public GameObject camA;
+
+    public GameObject goA;
+    public GameObject goB;
+
+   
+    private void Start()
+    {
+        print("哈囉，媽的發克??");
+        print(size);
+        print(brand);
+
+        weight = 1.3f;
+        haswindow = false;
+    }
+}
---
+++ 2DGame/Assets/script/TetrisManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class TetrisManager : MonoBehaviour
+{
+    [Header("掉落時間"),Range(0.1f,3)]
+    public float speed = 1.5f;
+    [Header("目前分數")]
+    public int score;
+    [Header("最高分數")]
+    public int highscore;
+    [Header("等級")]
+    public int level = 1;
+    [Header("結束畫面")]
+    public GameObject objFinal;
+    [Header("方塊掉落音效")]
+    public AudioClip sounddown;
+    [Header("方塊移動與旋轉音效")]
+    public AudioClip soundmove;
+    [Header("方塊消除音效")]
+    public AudioClip soundclip;
+    [Header("遊戲結束音效")]
+    public AudioClip soundgameover;
+}
